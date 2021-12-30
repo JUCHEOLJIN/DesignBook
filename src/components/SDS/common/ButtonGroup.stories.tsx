b055ef1f181be0616a12301a9be4904fa3b9397e
eden.ju <toto006521@gmail.com>
2021-12-30 17:15:48 +0900

    feat: SDS 버튼 그룹 컴포넌트 생성

---
+++ src/components/SDS/common/ButtonGroup.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import ButtonGroup from './ButtonGroup';
+import Button from './Button';
+
+export default {
+  component: ButtonGroup,
+  title: 'SDS/ButtonGroup',
+  argTypes: {},
+} as ComponentMeta<typeof ButtonGroup>;
+
+const TemplateRow: ComponentStory<typeof ButtonGroup> = (args) => (
+  <ButtonGroup {...args}>
+    <Button theme="tertiary" width="110px">
+      취소
+    </Button>
+    <Button width="110px">확인</Button>
+  </ButtonGroup>
+);
+
+const TemplateCol: ComponentStory<typeof ButtonGroup> = (args) => (
+  <ButtonGroup {...args}>
+    <Button theme="tertiary">취소</Button>
+    <Button>확인</Button>
+  </ButtonGroup>
+);
+
+export const Default = TemplateRow.bind({});
+Default.args = {};
+
+export const RightAlign = TemplateRow.bind({});
+RightAlign.args = {
+  rightAlign: true,
+};
+
+export const Column = TemplateCol.bind({});
+Column.args = {
+  direction: 'column',
+};
+
+export const CustomGap = TemplateRow.bind({});
+CustomGap.args = {
+  gap: '3rem',
+};
+
+export const CustomGapColumn = TemplateCol.bind({});
+CustomGapColumn.args = {
+  direction: 'column',
+  gap: '3rem',
+};
---
+++ src/components/SDS/common/ButtonGroup.tsx
@@ -0,0 +1,66 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+interface ButtonGroupProps {
+  /**
+   * 버튼을 보여줄 방향
+   */
+  direction?: 'row' | 'column';
+  /**
+   * 버튼을 우측에 보여줍니다.
+   */
+  rightAlign?: boolean;
+  /**
+   * 버튼과 버튼사이의 간격을 설정합니다.
+   */
+  gap?: number | string;
+  /**
+   * 버튼 그룹에서 보여줄 버튼들
+   */
+  children: React.ReactNode;
+  /**
+   *  스타일 커스터마이징 하고싶을 때 사용
+   */
+  className?: string;
+}
+
+const ButtonGroup = ({
+  direction = 'row',
+  rightAlign,
+  children,
+  gap = '0.5rem',
+  className,
+}: ButtonGroupProps) => {
+  return (
+    <Wrapper
+      css={[gapStyle(direction, gap), rightAlign && rightAlignStyle]}
+      direction={direction}
+      className={className}
+    >
+      {children}
+    </Wrapper>
+  );
+};
+
+export default ButtonGroup;
+
+const Wrapper = styled.div<{ direction: string }>`
+  display: flex;
+  flex-direction: ${({ direction }) => direction};
+  ${(props) => props.css}
+`;
+
+const gapStyle = (direction: 'row' | 'column', gap: number | string) => {
+  const marginType = direction === 'row' ? 'margin-left' : 'margin-top';
+  return css({
+    'button + button': {
+      [marginType]: gap,
+    },
+  });
+};
+
+const rightAlignStyle = css`
+  justify-content: flex-end;
+`;
